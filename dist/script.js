99f24e03491522d5b08920c1177cd69d2cfe9d7c
Célio V. Júnior <celio_cjr@hotmail.com>
2023-02-17 16:07:43 -0300

    dev: using new Function constructor to convert a string to a number and testing their operators

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log("hello world!");
+const calculatorElement = document.getElementById("calculator");
+const resultElement = document.getElementById("result");
+function evaluate(expression) {
+    try {
+        return new Function(`return (${expression})`)();
+    }
+    catch (err) {
+        return null;
+    }
+}
+function toCalculate() {
+    const lines = calculatorElement.value.split(/\r?\n/).forEach((line) => {
+        console.log(evaluate(line));
+    });
+    console.log(lines);
+}
+calculatorElement.addEventListener("input", toCalculate);
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,19 @@
-console.log("hello world!")
+const calculatorElement = document.getElementById("calculator") as HTMLInputElement;
+const resultElement = document.getElementById("result") as HTMLElement;
+
+function evaluate(expression: string) {
+  try {
+    return new Function(`return (${expression})`)();
+  } catch (err) {
+    return null;
+  }
+}
+
+function toCalculate() {
+  const lines = calculatorElement.value.split(/\r?\n/).forEach((line) => {
+    console.log(evaluate(line));
+  });
+  console.log(lines);
+}
+
+calculatorElement.addEventListener("input", toCalculate);
