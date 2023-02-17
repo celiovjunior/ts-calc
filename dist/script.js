48a8ef2f58c74ae2f93c5e2d8b99fa279ef1f8da
Célio V. Júnior <celio_cjr@hotmail.com>
2023-02-17 16:40:54 -0300

    feat: created tisNumber function validate the type number from the input and round function to round float result

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,22 @@
 "use strict";
 const calculatorElement = document.getElementById("calculator");
 const resultElement = document.getElementById("result");
+function round(value) {
+    return Math.round(value * 1000) / 1000;
+}
+function isNumber(value) {
+    if (typeof value === "number") {
+        return !isNaN(value) && isFinite(value);
+    }
+    else {
+        return false;
+    }
+}
 function evaluate(expression) {
     try {
+        if (expression.match(/[a-zA-Z&#$<>{}]/g))
+            throw new Error();
+        // Function constructor
         return new Function(`return (${expression})`)();
     }
     catch (err) {
@@ -10,10 +24,8 @@ function evaluate(expression) {
     }
 }
 function toCalculate() {
-    const lines = calculatorElement.value.split(/\r?\n/).forEach((line) => {
-        console.log(evaluate(line));
-    });
-    console.log(lines);
+    const lines = calculatorElement.value.split(/\r?\n/).map(evaluate);
+    resultElement.innerHTML = `<div> ${lines.map(l => `<div>${isNumber(l) ? round(l) : '---'}</div>`).join('')} </div>`;
 }
 calculatorElement.addEventListener("input", toCalculate);
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,22 @@
 const calculatorElement = document.getElementById("calculator") as HTMLInputElement;
 const resultElement = document.getElementById("result") as HTMLElement;
 
+function round(value: number) {
+  return Math.round(value * 1000) / 1000;
+}
+
+function isNumber(value: unknown): value is number {
+  if(typeof value === "number") {
+    return !isNaN(value) && isFinite(value);
+  } else {
+    return false;
+  }
+}
+
 function evaluate(expression: string) {
   try {
+    if (expression.match(/[a-zA-Z&#$<>{}]/g)) throw new Error();
+    // Function constructor
     return new Function(`return (${expression})`)();
   } catch (err) {
     return null;
@@ -10,10 +24,11 @@ function evaluate(expression: string) {
 }
 
 function toCalculate() {
-  const lines = calculatorElement.value.split(/\r?\n/).forEach((line) => {
-    console.log(evaluate(line));
-  });
-  console.log(lines);
+  const lines = calculatorElement.value.split(/\r?\n/).map(evaluate);
+
+  resultElement.innerHTML = `<div> ${lines.map(l => `<div>${isNumber(l) ? round(l) : '---'}</div>`).join('')} </div>`;
+
+  
 }
 
 calculatorElement.addEventListener("input", toCalculate);
